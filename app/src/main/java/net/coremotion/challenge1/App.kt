f866245248fd5bc87b4a3b351c8b9029b7e9d4ed
GiorgiDolidzee <giorgi.dolidze.2@btu.edu.ge>
2021-11-03 11:02:44 +0400

    Initial commit

---
+++ app/src/main/java/net/coremotion/challenge1/App.kt
@@ -0,0 +1,9 @@
+package net.coremotion.challenge1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+}
---
+++ app/src/main/java/net/coremotion/challenge1/data/remote/ApiService.kt
@@ -0,0 +1,17 @@
+package net.coremotion.challenge1.data.remote
+
+import net.coremotion.challenge1.common.ApiEndpoints
+import net.coremotion.challenge1.domain.model.Users
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET(ApiEndpoints.USERS)
+    suspend fun getUsers(@Query("page") page: Int): Response<Users>
+
+    @GET(ApiEndpoints.USER_DETAIL)
+    suspend fun getUserDetail(@Path("id") id: Int): Response<Users.Data>
+
+}
